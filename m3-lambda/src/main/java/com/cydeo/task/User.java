6d2d9f081145f7ff09f865a05f38d097c8d506f4
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-17 23:32:20 +0300

    OrangeTest-Person-User-UserTest

---
+++ m3-lambda/src/main/java/com/cydeo/task/User.java
@@ -0,0 +1,14 @@
+package com.cydeo.task;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data //@Getter @Setter @ToString
+@AllArgsConstructor
+public class User {
+
+    private String firstName;
+    private String lastname;
+    private int age;
+
+}
