0c9aa935adeb36fbab98567bacfd5556d2c5574d
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-18 16:08:48 +0300

    Transaction.java

---
+++ m5-stream/src/main/java/com/cydeo/task/Transaction.java
@@ -0,0 +1,15 @@
+package com.cydeo.task;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Transaction {
+
+    private Trader trader;
+    private int year;
+    private int value;
+
+
+}
